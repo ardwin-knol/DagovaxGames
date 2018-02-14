b095a52f39b107676a9db5f0df983292705ec8cd
ardwin-knol <ardwinknol@hotmail.com>
2018-02-14 14:07:57 +0100

    Changed home page layour + added widget layout

--- js/feedback_widget/feedback_widget.js
+++ js/feedback_widget/feedback_widget.js
@@ -1,44 +1,48 @@
-function FeedbackWidget(wrapper_id) {
-    this.wrapper_id = '#' + wrapper_id;
-    this.isInit = false;
-    this.show = show;
-    this.hide = hide;
-    this.init = init;
+// function FeedbackWidget(wrapper_id) {
+//     this.wrapper_id = '#' + wrapper_id;
+//     this.isInit = false;
+//     this.show = show;
+//     this.hide = hide;
+//     this.init = init;
+//
+//     function init(id) {
+//         this.isInit = true;
+//         var feedback_id = '#' + id;
+//         var visible = false;
+//
+//         $(this.wrapper_id).append($('<img>',
+//             {
+//                 id: 'feedback-image',
+//                 src: '../resources/feedback-image'
+//             }));
+//
+//         $(this.wrapper_id).on('click', function (e) {
+//             e.stopPropagation();
+//             if(visible){
+//                 $(feedback_id).hide();
+//                 visible = false;
+//             }else{
+//                 $(feedback_id).show();
+//                 visible = true;
+//             }
+//
+//         });
+//     };
+//
+//     function show() {
+//         $(this.feedback_id).fadeIn();
+//     }
+//
+//     function hide() {
+//         $(this.feedback_id).hide();
+//     }
+// }
+//
+// $(function () {
+//     var feedbackWidget = new FeedbackWidget('wrapper');
+//     feedbackWidget.init('feedback-widget');
+// });
 
-    function init(id) {
-        this.isInit = true;
-        var feedback_id = '#' + id;
-        var visible = false;
+$(function FeedbackWidget(){
 
-        $(this.wrapper_id).append($('<img>',
-            {
-                id: 'feedback-image',
-                src: '../resources/feedback-image'
-            }));
-
-        $(this.wrapper_id).on('click', function (e) {
-            e.stopPropagation();
-            if(visible){
-                $(feedback_id).hide();
-                visible = false;
-            }else{
-                $(feedback_id).show();
-                visible = true;
-            }
-
-        });
-    };
-
-    function show() {
-        $(this.feedback_id).fadeIn();
-    }
-
-    function hide() {
-        $(this.feedback_id).hide();
-    }
-}
-
-$(function () {
-    var feedbackWidget = new FeedbackWidget('wrapper');
-    feedbackWidget.init('feedback-widget');
-});
+})
