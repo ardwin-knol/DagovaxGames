0cd3cc3372a26d9ea55651f663182422565ba874
ardwin-knol <ardwinknol@hotmail.com>
2018-02-15 15:28:19 +0100

    Added working widget and changed some layout

--- js/feedback_widget/feedback_widget.js
+++ js/feedback_widget/feedback_widget.js
@@ -1,124 +1,82 @@
-function FeedbackWidget(wrapper_id) {
-    this.wrapper_id = '#' + wrapper_id;
-    this.isInit = false;
-    this.open = open;
-    this.close = close;
-    this.init = init;
-
-    function init(id) {
-        this.isInit = true;
-        var feedback_id = '#' + id;
-        var isAnimated = false;
-
-        $(this.wrapper_id).on('click', function (e) {
-            e.stopPropagation();
-            if (!isAnimated) {
-                (feedback_id).open();
-                isAnimated = true;
-            }
-            else if (isAnimated) {
-                (feedback_id).close();
-                isAnimated = false;
-            }
-        });
-    }
-
-    function open() {
-            $(this.feedback_id).animate({
-                'left': '75%',
-            }, 1000);
-    }
-
-    function close() {
-            $(this.feedback_id).animate({
-                'left': '97.5%',
-            }, 1000);
-    }
-
-    $(function () {
-        var feedbackWidget = new FeedbackWidget('widget-content');
-        feedbackWidget.init('widget-content');
-    });
-};
-
-
-
-
-
-
-
-
-
-// function FeedbackWidget(wrapper_id) {
-//     this.wrapper_id = '#' + wrapper_id;
+// function FeedbackWidget(image_id) {
+//     this.image_id = '#' + image_id;
 //     this.isInit = false;
-//     this.show = show;
-//     this.hide = hide;
+//     this.open = open;
+//     this.close = close;
 //     this.init = init;
 //
 //     function init(id) {
 //         this.isInit = true;
 //         var feedback_id = '#' + id;
-//         var visible = false;
+//         var isAnimated = false;
 //
-//         $(this.wrapper_id).append($('<img>',
-//             {
-//                 id: 'feedback-image',
-//                 src: '../resources/feedback-image'
-//             }));
-//
-//         $(this.wrapper_id).on('click', function (e) {
-//             e.stopPropagation();
-//             if(visible){
-//                 $(feedback_id).hide();
-//                 visible = false;
-//             }else{
-//                 $(feedback_id).show();
-//                 visible = true;
+//         $(this.image_id).on('click', function () {
+//             if (!isAnimated) {
+//                 $feedback_id.open();
+//                 isAnimated = true;
+//                 console.log(isAnimated);
+//                 console.log('showing widget...');
 //             }
-//
+//             else if (isAnimated) {
+//                 $feedback_id.close();
+//                 isAnimated = false;
+//                 console.log(isAnimated);
+//                 console.log('hiding widget...');
+//             }
 //         });
-//     };
-//
-//     function show() {
-//         $(this.feedback_id).fadeIn();
 //     }
 //
-//     function hide() {
-//         $(this.feedback_id).hide();
+//     function open() {
+//             $(this.feedback_id).animate({
+//                 'left': '75%'
+//             }, 1000);
 //     }
-// }
 //
-// $(function () {
-//     var feedbackWidget = new FeedbackWidget('wrapper');
-//     feedbackWidget.init('feedback-widget');
-// });
-
-// $(document).ready(function FeedbackWidget(){
+//     function close() {
+//             $(this.feedback_id).animate({
+//                 'left': '97.5%'
+//             }, 1000);
+//     }
 //
-//     $('#widget-content > #widget').on('click', function (){
-//         var isClicked = false;
-//         if (!isClicked) {
-//             var col =$('#widget-content')
-//             col
-//                 .css({
-//                     'left': '75%'
-//                 })
-//                 .animate({
-//                     'left': '75%'
-//                 });
-//             isClicked = true;
-//         }
-//         else{
-//             var col =$('#widget-content')
-//             col
-//                 .css({
-//                     'left' : '97.5%'
-//                 })
-//                 .animate({
-//                     'left' : '97.5%'
-//                 });
-//             isClicked = false;
-//             }
+//     $(function () {
+//         var feedbackWidget = new FeedbackWidget('widget-content');
+//         feedbackWidget.init('top-image');
 //     });
-// });
+// };
+
+
+
+$(document).ready(function FeedbackWidget(){
+    //removes availabilty of the user to drag the images
+    $('img').on('dragstart', function(event) { event.preventDefault(); });
+
+    var isClicked = false;
+
+    $('#top-image').click(function (){
+        open();
+    });
+
+    $('#widget-top img').click(function (){
+        close();
+    });
+
+    function close() {
+            $('#widget-content').animate({
+                'left': '97.5%'
+            }, 1000);
+            $('#top-image img').animate({
+                opacity:1
+            }, 1000);
+            isClicked = false;
+    }
+
+    function open() {
+            $('#widget-content').animate({
+                'left': '75%'
+            }, 1000);
+            $('#top-image img').animate({
+                opacity:0
+            }, 1000);
+            isClicked = true;
+    }
+});
